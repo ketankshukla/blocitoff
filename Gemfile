98d4fd2201939817c645e6c88eec02d9b70ead00
Ketan Shukla <ketankshukla@gmail.com>
2015-09-28 13:22:08 -0700

    Added figaro gem for seed data and mark to-do items as complete

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'devise'
 
 gem 'bootstrap-sass'
 
+gem 'faker'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create users
+5.times do
+  user = User.new(
+      name:     Faker::Name.name,
+      email:    Faker::Internet.email,
+      password: "helloworld"
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
+# Create items
+25.times do
+  todo = Todo.create(
+      user: users.sample,
+      name: Faker::Lorem.sentence
+  )
+  todo.update_attribute(:created_at, rand(10.minutes .. 6.days).ago)
+end
+
+user = User.new(
+    name:     "Ketan Shukla",
+    email:    "ketankshukla@gmail.com",
+    password: "password"
+)
+user.skip_confirmation!
+user.save
+
+puts "#{User.count} users created"
+puts "#{Todo.count} items created"
