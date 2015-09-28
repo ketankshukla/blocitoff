5bec161aad74741d784863d950e5f9884b02c6de
Ketan Shukla <ketankshukla@gmail.com>
2015-09-28 14:45:32 -0700

    Configured local and Heroku mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,20 @@
 Rails.application.configure do
+
+  #### USING MANDRILL MAILER FOR LOCAL DEPLOYMENT
+
+  ActionMailer::Base.smtp_settings = {
+      address: "smtp.mandrillapp.com",
+      port: 587,
+      enable_starttls_auto: true,
+      user_name: "ketankshukla@gmail.com",
+      password: "x5XT7AIu1J5peiHu_L_OJg",
+      authentication: "login"
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.default charset: "utf-8"
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
