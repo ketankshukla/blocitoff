ebec00dc085d8dc2e3d05542d528ec4c12ee4169
Ketan Shukla <ketankshukla@gmail.com>
2015-09-28 15:02:26 -0700

    Fixed time remaining on todo task

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,7 @@ class Todo < ActiveRecord::Base
   belongs_to :user
 
   def days_left
-    (DateTime.now.to_date - created_at.to_date).to_i
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
   end
 
 end
