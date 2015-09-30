57c4f94f884778451f95b76ab7ef82319d0ca11e
Ketan Shukla <ketankshukla@gmail.com>
2015-09-29 22:26:04 -0700

    Ordered the Todos with most recent on top

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,7 @@ class Todo < ActiveRecord::Base
 
   include ActionView::Helpers::DateHelper
   belongs_to :user
-  default_scope { order('updated_at DESC') }
+  default_scope { order('created_at DESC') }
 
   def days_left
     distance_of_time_in_words(7 - (Time.now - created_at.to_time))
